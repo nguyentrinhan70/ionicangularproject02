064da391d075b5e554eeb11e310988247b740596
Trí Nhân Nguyễn <nguyentrinha70@pdu.edu.vn>
2020-06-01 14:21:12 +0700

    01-ionic angular part 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  title = 'This is my ionic angular 2020 tutorial s series!';
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
